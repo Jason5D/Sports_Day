b372cc0a48efcccc37713dc07b8b7a96ce47a97b
Jason <jason5donoghue@gmail.com>
2023-07-18 21:13:12 +0100

    new page

--- sportsday/src/Navbar.js
+++ sportsday/src/Navbar.js
@@ -5,6 +5,7 @@ export default function Navbar () {
         <h2 className="Home">Home</h2>
         <h2 className="Year">This Year</h2>
         <h2 className="Heroes">Hall of Heroes</h2>
+        <h2 className="Gallery">Gallery</h2>
         </div>
         </div>
     )
